d1a17cff58c16eac92ca1de0bbd87d3ae8bde5eb
mmvalentin1 <mettemariie@gmail.com>
2021-01-22 11:23:04 +0100

    changes to the order form

--- store.js
+++ store.js
@@ -1,104 +1,60 @@
-if (document.readyState == 'loading') {
-  document.addEventListener('DOMContentLoaded', ready)
-} else {
-  ready()
+const endPoint = "https://foobar32.herokuapp.com/";
+
+window.addEventListener("load", init);
+
+function init() {
+  fetch(endPoint)
+    .then((res) => res.json())
+    .then(buildDOM);
 }
 
-function ready() {
-  var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-  for (var i = 0; i < removeCartItemButtons.length; i++) {
-      var button = removeCartItemButtons[i]
-      button.addEventListener('click', removeCartItem)
-  }
+setInterval(() => {
+  //document.querySelector("#taps").innerHTML = "";
+  fetch(endPoint)
+    .then((res) => res.json())
+    .then(updateDOM);
+}, 4000);
 
-  var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-  for (var i = 0; i < quantityInputs.length; i++) {
-      var input = quantityInputs[i]
-      input.addEventListener('change', quantityChanged)
-  }
-
-  var addToCartButtons = document.getElementsByClassName('shop-item-button')
-  for (var i = 0; i < addToCartButtons.length; i++) {
-      var button = addToCartButtons[i]
-      button.addEventListener('click', addToCartClicked)
-  }
-
-  document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+function buildDOM(data) {
+  //console.log(data);
+  buildTaps(data.storage);
 }
 
-
-function purchaseClicked() {
-  alert('Thank you for your purchase')
-  var cartItems = document.getElementsByClassName('cart-items')[0]
-  while (cartItems.hasChildNodes()) {
-      cartItems.removeChild(cartItems.firstChild)
-  }
-  updateCartTotal()
+function updateDOM(data) {
+  updateTaps(data.storage);
+}
+function updateTaps(beers) {
+  beers.forEach((beer) => {
+    const element = document.querySelector(`.beer[data-beername='${beer.name}']`);
+   /*  element.querySelector(
+      "p"
+    ).textContent = `${tap.level} out of ${tap.capacity}`;
+    //console.log(element);
+    element.querySelector(".inner").style.width =
+      (tap.level / tap.capacity) * 100 + "%"; */
+  });
+}
+function buildTaps(beers) {
+  //console.log(taps);
+  //grab the template
+  const template = document.querySelector("#storageTemp").content;
+  //loop through the taps
+  beers.forEach((beer) => {
+    //console.log(tap);
+    //3 clone the taps
+    const myBeer = template.cloneNode(true);
+    myBeer.querySelector("article").dataset.beername = beer.name;
+    //4 popultate
+    myBeer.querySelector("h2").textContent = beer.name;
+    myBeer.querySelector("img").setAttribute("src", "Images" + beer.image + "-sm.png")
+    /* myTap.querySelector(
+      "p"
+    ).textContent = `${tap.level} out of ${tap.capacity}`;
+    //5 append to DOM
+    myTap.querySelector(".inner").style.width =
+      (tap.level / tap.capacity) * 100 + "%"; */
+    document.querySelector("#beers").appendChild(myBeer);
+  });
 }
 
-function removeCartItem(event) {
-  var buttonClicked = event.target
-  buttonClicked.parentElement.parentElement.remove()
-  updateCartTotal()
-}
-
-function quantityChanged(event) {
-  var input = event.target
-  if (isNaN(input.value) || input.value <= 0) {
-      input.value = 1
-  }
-  updateCartTotal()
-}
-
-function addToCartClicked(event) {
-  var button = event.target
-  var shopItem = button.parentElement.parentElement
-  var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-  var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-  var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-  addItemToCart(title, price, imageSrc)
-  updateCartTotal()
-}
-
-function addItemToCart(title, price, imageSrc) {
-  var cartRow = document.createElement('div')
-  cartRow.classList.add('cart-row')
-  var cartItems = document.getElementsByClassName('cart-items')[0]
-  var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-  for (var i = 0; i < cartItemNames.length; i++) {
-      if (cartItemNames[i].innerText == title) {
-          alert('There is already one item added to the cart')
-          return
-      }
-  }
-  var cartRowContents = `
-      <div class="cart-item cart-column">
-          <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-          <span class="cart-item-title">${title}</span>
-      </div>
-      <span class="cart-price cart-column">${price}</span>
-      <div class="cart-quantity cart-column">
-          <input class="cart-quantity-input" type="number" value="1">
-          <button class="btn btn-danger" type="button">REMOVE</button>
-      </div>`
-  cartRow.innerHTML = cartRowContents
-  cartItems.append(cartRow)
-  cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-  cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
-}
-
-function updateCartTotal() {
-  var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-  var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-  var total = 0
-  for (var i = 0; i < cartRows.length; i++) {
-      var cartRow = cartRows[i]
-      var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-      var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-      var price = parseFloat(priceElement.innerText.replace('$', ''))
-      var quantity = quantityElement.value
-      total = total + (price * quantity)
-  }
-  total = Math.round(total * 100) / 100
-  document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
-}
+copy.querySelector("img").setAttribute("src", "imgs/small/" + dish.image + "-sm.jpg");
